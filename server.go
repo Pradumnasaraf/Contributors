f1ec39816fc144900764e95f436510ed8b88dcb8
Pradumna saraf <pradumnasaraf@gmail.com>
2023-05-26 16:23:13 +0530

    feat: add gin basicauth

--- server.go
+++ server.go
@@ -4,9 +4,9 @@ import (
 	"log"
 
 	"github.com/gin-gonic/gin"
-	"github.com/pradumnasaraf/go-api/handler"
 	"github.com/pradumnasaraf/go-api/config"
-
+	"github.com/pradumnasaraf/go-api/handler"
+	"github.com/pradumnasaraf/go-api/middleware"
 )
 
 func main() {
@@ -15,8 +15,10 @@ func main() {
 
 	router := gin.Default()
 
+	router.Use(middleware.BasicAuth())
 	router.GET("/", handler.PlaygroundHandler())
 	router.POST("/query", handler.GraphqlHandler())
 
+	// Auto catch the PORT variable if it exists
 	log.Fatal(router.Run())
 }
