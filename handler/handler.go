02e5d56fc2b43f1fc48b69f394ef05cb862d77b6
Pradumna Saraf <pradumnasaraf@gmail.com>
2024-10-23 16:12:26 +0530

    feat: add a redis rate limiter

--- handler/handler.go
+++ handler/handler.go
@@ -1,10 +1,14 @@
 package handler
 
 import (
+	"net/http"
+
 	"github.com/99designs/gqlgen/graphql/handler"
 	"github.com/99designs/gqlgen/graphql/playground"
 	"github.com/Pradumnasaraf/Contributors/graph"
 	"github.com/gin-gonic/gin"
+
+	"github.com/Pradumnasaraf/Contributors/middleware"
 )
 
 // Graphql handler
@@ -12,8 +16,12 @@ func GraphqlHandler() gin.HandlerFunc {
 	// NewExecutableSchema and Config are in the generated.go file
 	// Resolver is in the resolver.go file
 	h := handler.NewDefaultServer(graph.NewExecutableSchema(graph.Config{Resolvers: &graph.Resolver{}}))
-
 	return func(c *gin.Context) {
+		err := middleware.RateLimiter(c.ClientIP())
+		if err != nil {
+			c.JSON(http.StatusTooManyRequests, gin.H{"error": "Too Many Requests"})
+			return
+		}
 		h.ServeHTTP(c.Writer, c.Request)
 	}
 }
@@ -21,7 +29,6 @@ func GraphqlHandler() gin.HandlerFunc {
 // Playground handler
 func PlaygroundHandler() gin.HandlerFunc {
 	h := playground.Handler("GraphQL", "/query")
-
 	return func(c *gin.Context) {
 		h.ServeHTTP(c.Writer, c.Request)
 	}
--- middleware/basicAuth.go
+++ middleware/basicAuth.go
@@ -3,13 +3,10 @@ package middleware
 import (
 	"os"
 
-	"github.com/Pradumnasaraf/Contributors/config"
 	"github.com/gin-gonic/gin"
 )
 
 func BasicAuth() gin.HandlerFunc {
-
-	config.Config()
 
 	username := os.Getenv("BASIC_AUTH_USERNAME")
 	password := os.Getenv("BASIC_AUTH_PASSWORD")
